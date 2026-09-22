77d874f203ce9f2ca1f1b39281303cc26354d44f
Esteban Wytrykusz <esteban.wytrykusz10@gmail.com>
2026-09-22 20:51:15 -0300

    Se agrega las carpetas js y pages, y se crea el login para el dashboard

---
+++ js/login.js
@@ -0,0 +1,22 @@
+const ADMIN_USER = "admin";
+const ADMIN_PASS = "Prog4Com2";
+
+const loginForm = document.getElementById("login-form");
+const userInput = document.getElementById("user");
+const passwordInput = document.getElementById("password");
+const errorMessage = document.getElementById("error-message");
+
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const userIngresado = userInput.value.trim();
+  const passwordIngresado = passwordInput.value;
+
+  if (userIngresado === ADMIN_USER && passwordIngresado === ADMIN_PASS) {
+    localStorage.setItem("adminLogueado", "true");
+    window.location.href = "../pages/dashboard.html";
+  } else {
+    errorMessage.textContent = "Usuario o contraseÃ±a incorrectos.";
+    errorMessage.style.display = "block";
+  }
+});
